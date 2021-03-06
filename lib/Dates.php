43f6a1d78c3a949918d1c8f8274da895c049b5fb
nnamuncura <nnamuncura@icloud.com>
2021-03-06 02:01:42 -0300

    se creo un respaldo de la carpeta php-apps-prof

---
+++ lib/Dates.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace lib;
+
+use Carbon\Carbon;
+
+class Dates {
+
+    public static function today() {
+        return Carbon::now();
+    }
+    
+    public static function tomorrow(){
+        return self::today()->addDays(1);
+    }
+    
+    public static function longDate($date){
+        Carbon::setLocale('es');
+        return $date->isoFormat('dddd DD [de] MMMM');
+    }
+
+}
---
+++ public/index.php
@@ -0,0 +1,50 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+
+    <title>Hello, world!</title>
+  </head>
+  <body>
+    <h1>Hello, world!</h1>
+<?php
+require '../vendor/autoload.php';
+
+use Carbon\Carbon;
+use lib\Dates;
+
+$date = Carbon::now();
+echo $date->format('Y');
+
+Carbon::setLocale('es');
+$today = Carbon::now();
+$tomorrow = $today->addDays(1);
+echo $tomorrow->isoFormat('dddd DD [de] MMMM');
+//include '../lib/Dates.php';
+
+?>
+    <p>Con php es fácil hacer spaghetti code y mezclar la presentación, el html con código </p>
+    <p>
+        pero en 
+        <?= Dates::longDate(Dates::today()) ?> 
+        lo vamos a solucionar.
+    </p>
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.6.0/dist/umd/popper.min.js" integrity="sha384-KsvD1yqQ1/1+IA7gi3P0tyJcT3vR+NdBTt13hSJ2lnve8agRGXTTyNaBYmCR/Nwi" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.min.js" integrity="sha384-nsg8ua9HAw1y0W1btsyWgBklPnCUAFLuTMS2G72MMONqmOymq585AcH49TLBQObG" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
+
+
